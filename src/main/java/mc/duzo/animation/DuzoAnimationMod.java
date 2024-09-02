086fcc21b52b161ba641d1d5dba7380716afccab
James Hall <james.hall01@outlook.com>
2024-09-02 18:11:07 +0100

    chore: initial blank commit

---
+++ src/main/java/mc/duzo/animation/DuzoAnimationMod.java
@@ -0,0 +1,16 @@
+package mc.duzo.animation;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DuzoAnimationMod implements ModInitializer {
+    public static final String MOD_ID = "animation";
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/mc/duzo/animation/datagen/AnimationDataGenerator.java
@@ -0,0 +1,11 @@
+package mc.duzo.animation.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AnimationDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+    }
+}
